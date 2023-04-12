745b11eeb9ae4bc80ed397508c2c4657e940b510
Olga Karzhenka <korolga.lik@gmail.com>
2023-04-12 16:26:53 -0700

    added API key

--- src/components/controller/appLoader.js
+++ src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '97643816ca8048238857657dcd7820e7', // получите свой ключ https://newsapi.org/
         });
     }
 }
